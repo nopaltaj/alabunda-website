03e01bad48e09029d51827d219da3de2a0f4a63c
nopaltaj <tajdhiya.arkan@gmail.com>
2024-05-02 13:23:46 +0700

    Responsive

--- database/migrations/2024_03_06_134606_create_categories_table.php
+++ database/migrations/2024_03_06_134606_create_categories_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
 
             $table->string('name');
             $table->string('image');
-            $table->string('description');
+            $table->longText('description');
             
             $table->timestamps();
         });
